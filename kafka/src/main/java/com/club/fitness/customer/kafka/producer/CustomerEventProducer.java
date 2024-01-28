be9833ccc4317c2c2947274a827c64954baa2a0a
Aliaksandr Yafremenka <Aliaksandr_Yafremenka@epam.com>
2024-01-28 21:15:48 +0400

    add zipkin add sleuth to tracing logs

--- kafka/src/main/java/com/club/fitness/customer/kafka/producer/CustomerEventProducer.java
+++ kafka/src/main/java/com/club/fitness/customer/kafka/producer/CustomerEventProducer.java
@@ -1,5 +1,7 @@
 package com.club.fitness.customer.kafka.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -8,6 +10,8 @@ import com.club.fitness.customer.kafka.event.CustomerEvent;
 
 @Service
 public class CustomerEventProducer {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CustomerEventProducer.class);
 	
 	private final KafkaTemplate<String, CustomerEvent> kafkaTemplate;
 	private final String topicName;
@@ -19,7 +23,13 @@ public class CustomerEventProducer {
 	}
 	
 	public void sendCustomerEvent(final CustomerEvent customerEvent) {
-		kafkaTemplate.send(topicName, customerEvent);
+		logger.debug("sending customer event: {} to the following topic: {}", customerEvent, topicName);
+		final var future = kafkaTemplate.send(topicName, customerEvent);
+		future.addCallback(event -> logger.debug("sending customer event: {} is successfull", event.getProducerRecord().value()),
+						ex -> {
+							logger.debug("sending customer event: {} is failure. {}", customerEvent, ex);
+							throw new RuntimeException("sending customer event: " + customerEvent + " is failure.");
+						});
 	}
 	
 	
